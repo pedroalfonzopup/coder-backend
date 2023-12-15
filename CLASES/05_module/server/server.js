08007cab6f94a9c284e8cd4847c614167eec9250
Pedro Alfonzo <pedroalfonzopup@gmail.com>
2023-12-14 22:38:35 -0300

    acomodamiento

--- CLASES/05_module/server/server.js
+++ CLASES/05_module/server/server.js
@@ -2,4 +2,15 @@
 const http = require("http")
 //REQUIERO EL MODULO NECESARIO PARA GESTIONAR SERVIDORES (Crearlo, configurarlo y levantarlo)
 
-const server = http.createServer
+const server = http.createServer()
+//.createServer() es el motodo para crear un servidor
+
+//PARA INICIARLO NECESITO:
+    //PUERTO: (8080)
+    //CALLBACK para informarnos de que el servidor esta funcionando
+
+const PORT = 8080
+const cbReady = () => console.log("Servidor listorti en "+PORT)
+
+server.listen(PORT, cbReady)
+//listen() es el metodo necesario para iniciarlo
